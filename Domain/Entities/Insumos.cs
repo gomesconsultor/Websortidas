d1b5ab8586b00f3fa8aa565d393ced8c911cf02d
Carlos <gomescgconsultoria@gmail.com>
2020-06-15 11:01:14 -0300

    Inclusao de insumos

---
+++ Domain/Entities/Insumos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSortidas.Domain.Entities
+{
+    public class Insumos: Entity
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Unity { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
